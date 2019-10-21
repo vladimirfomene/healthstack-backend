6938d3391c7013ecbd4d4478a116d68c0043f4e2
vladimirfomene <vladimirfomene@gmail.com>
2019-10-21 10:10:56 +0100

    implement CRU controller methods for labrequest model

---
+++ controllers/labrequest.js
@@ -0,0 +1,103 @@
+const labrequests = require('../models/labrequest');
+
+
+exports.createLabRequest = (req, res, next) => {
+    labrequests.createLabRequest(req.body.labRequest)
+    then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest creation failed';
+        return next(err);
+    });
+};
+
+exports.getLabRequestById = (req, res, next) => {
+    labrequests.getLabRequestById(req.params.id)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest query failed';
+        return next(err);
+    });
+};
+
+exports.updateLabRequest = (req, res, next) => {
+    labrequests.updateLabRequest(req.body.labRequest)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest update failed';
+        return next(err);
+    });
+};
+
+let getLabRequestByEmail = (req, res, next) => {
+    labrequests.getLabRequestByEmail(req.query.email)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest query failed';
+        return next(err);
+    });
+};
+
+let getLabRequestByName = (req, res, next) => {
+    labrequests.getLabRequestByName(req.query.name)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest query failed';
+        return next(err);
+    });
+
+};
+
+let getLabRequestByPhoneNumber = (req, res, next) => {
+    labrequests.getLabRequestByPhoneNumber(req.query.tel)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest query failed';
+        return next(err);
+    });
+};
+
+exports.getLabRequests = (req, res, next) => {
+    if(req.query.email) return getLabRequestByEmail(req, res, next);
+    if(req.query.tel) return getLabRequestByPhoneNumber(req, res, next);
+    if(req.query.name) return getLabRequestByName(req, res, next);
+
+    return getAllLabRequests(req, res, next);
+};
+
+let getAllLabRequests = (req, res, next) => {
+    labrequests.getLabRequests()
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'labrequest query failed';
+        return next(err);
+    });
+};
+
