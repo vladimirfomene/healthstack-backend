4ec9ecd3948a9be8ff4e58855c6cb0e458f8dd68
vladimirfomene <vladimirfomene@gmail.com>
2019-10-21 10:13:36 +0100

    implement CRU controller methods for patient model

---
+++ controllers/patient.js
@@ -0,0 +1,86 @@
+const patients = require('../models/patient');
+
+exports.getPatientById = (req, res, next) => {
+    patients.getPatientById(req.params.id)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
+
+exports.createPatient = (req, res, next) => {
+    patients.createPatient(req.body.patient)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
+
+exports.getPatients = (req, res, next) => {
+    if(req.query.name) return getPatientByName(req, res, next);
+    if(req.query.email) return getPatientByEmail(req, res, next);
+    if(req.query.tel) return getPatientByPhoneNumber(req, res, next);
+    return getAllPatients(req, res, next);
+};
+
+let getPatientByName = (req, res, next) => {
+    patients.getPatientByName(req.query.name)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
+
+let getPatientByEmail = (req, res, next) => {
+    patients.getPatientByEmail(req.query.email)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
+
+let getPatientByPhoneNumber = (req, res, next) => {
+    patients.getPatientByPhoneNumber(req.query.tel)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
+
+let getAllPatients = (req, res, next) => {
+    patients.getPatients()
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'patient query failed';
+        return next(err);
+    });
+};
