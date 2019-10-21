84e66a7dd315ae3de209d70fc8a89f7e74d725b3
vladimirfomene <vladimirfomene@gmail.com>
2019-10-21 10:08:10 +0100

    implement CRU controller for exam model

---
+++ controllers/exam.js
@@ -0,0 +1,72 @@
+const exams = require('../models/exam');
+
+
+exports.getExamById = (req, res, next) => {
+    exams.getExamById(req.params.id)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'exam query failed';
+        return next(err);
+    });
+};
+
+exports.createExam = (req, res, next) => {
+    exams.createExam(req.body.exam)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'exam query failed';
+        return next(err);
+    });
+};
+
+exports.updateExam = (req, res, next) => {
+    exams.updateExam(req.body.exam)
+    .then(resp => {
+        if(!resp._id) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'exam query failed';
+        return next(err);
+    });
+};
+
+let getAllExams = (req, res, next) => {
+    exams.getExams()
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'exam query failed';
+        return next(err);
+    });
+};
+
+let getExamByName = (req, res, next) => {
+    exams.getExamByName(req.query.name)
+    .then(resp => {
+        if(!resp.rows.length) return res.status(404).json({ msg: 'Not Found'});
+        return res.status(200).json(resp.rows);
+    })
+    .catch(err => {
+        err.status = 500;
+        err.msg = 'exam query failed';
+        return next(err);
+    });
+};
+
+exports.getExams = (req, res, next) => {
+    if(req.query.name) return getExamByName(req, res, next);
+    return getAllExams(req, res, next);
+};
