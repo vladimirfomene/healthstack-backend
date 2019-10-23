4466ff2c1c35563ef01b8d152c102805e9d5bed5
vladimirfomene <vladimirfomene@gmail.com>
2019-10-23 18:26:15 +0100

    rewrite laboratory model methods using couchbase sdk

--- models/laboratory.js
+++ models/laboratory.js
@@ -1,17 +1,21 @@
+const util = require('util');
 const { DB_HOST, DB_NAME } = require('../config/database_setup');
-const nano = require('nano')(DB_HOST);
-const db = nano.db.use(DB_NAME);
+const couchbase = require('couchbase');
+const bucket = (new couchbase.Cluster(DB_HOST)).openBucket(DB_NAME);
 
 exports.getLaboratoryById = (id) => {
-    return db.get(id);
+    let get = util.promisify(bucket.get);
+    return get(id);
 };
 
 exports.createLaboratory = (laboratory) => {
-    return db.insert(laboratory);
+    let insert = util.promisify(bucket.insert);
+    return insert(laboratory.key, laboratory);
 }
 
 exports.updateLaboratory = (laboratory) => {
-    return db.atomic('_design/laboratory', 'inplace', laboratory._id, laboratory);
+    let upsert = util.promisify(bucket.upsert);
+    return upsert(laboratory.key, laboratory);
 };
 
 exports.addLaboratoryLogo = () => {
