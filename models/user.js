3b53534b2c06a74ad2dafdc0fb29325418ef03fc
vladimirfomene <vladimirfomene@gmail.com>
2019-10-23 18:29:25 +0100

    rewrite user model methods with couchbase sdk

--- models/user.js
+++ models/user.js
@@ -1,25 +1,30 @@
+const util = require('util');
 const { DB_HOST, DB_NAME } = require('../config/database_setup');
-const nano = require('nano')(DB_HOST);
-const db = nano.db.use(DB_NAME);
+const couchbase = require('couchbase');
+const bucket = (new couchbase.Cluster(DB_HOST)).openBucket(DB_NAME);
 
 exports.getUserByEmail = (email) => {
-    return db.view('_design/user', 'email-view', {
-        'key': email
-    });
+    let queryString = 'SELECT * FROM' +  DB_NAME + 'WHERE type=$1 AND LOWER(email) LIKE %$2%';
+    let query = util.promisify(bucket.query);
+    return query(couchbase.N1qlQuery.fromString(queryString), ['user', email.toLowerCase()]);
 };
 
 exports.getUserById = (id) => {
-    return db.get(id);
-};
+    let get = util.promisify(bucket.get);
+    return get(id);
 
 exports.getUsers = () => {
-    return db.view('_design/user', 'email-view');
+    let queryString = 'SELECT * FROM' +  DB_NAME + 'WHERE type=$1';
+    let query = util.promisify(bucket.query);
+    return query(couchbase.N1qlQuery.fromString(queryString), ['user']);
 };
 
 exports.createUser = (user) => {
-    return db.insert(user);
+    let insert = util.promisify(bucket.insert);
+    return insert(user.key, user);
 };
 
 exports.updateUser = (user) => {
-    return db.atomic('_design/user', 'inspace', user._id, user);
+    let upsert = util.promisify(bucket.upsert);
+    return upsert(user.key, user);
 };
