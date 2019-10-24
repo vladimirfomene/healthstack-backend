0dd6a233b9267f894406921131e6f10f53ced48b
vladimirfomene <vladimirfomene@gmail.com>
2019-10-24 17:15:46 +0100

    wrap user model in promises and update database config

--- models/user.js
+++ models/user.js
@@ -1,30 +1,52 @@
-const util = require('util');
 const { DB_HOST, DB_NAME } = require('../config/database_setup');
 const couchbase = require('couchbase');
-const bucket = (new couchbase.Cluster(DB_HOST)).openBucket(DB_NAME);
+const cluster = new couchbase.Cluster(DB_HOST);
+cluster.authenticate('Administrator', 'fomeneodiwuor');
+const bucket = cluster.openBucket(DB_NAME);
 
 exports.getUserByEmail = (email) => {
     let queryString = 'SELECT * FROM' +  DB_NAME + 'WHERE type=$1 AND LOWER(email) LIKE %$2%';
-    let query = util.promisify(bucket.query);
-    return query(couchbase.N1qlQuery.fromString(queryString), ['user', email.toLowerCase()]);
+    return new Promise((resolve, reject) => {
+        bucket.query(couchbase.N1qlQuery.fromString(queryString), ['user', email.toLowerCase()], (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+    });
 };
 
 exports.getUserById = (id) => {
-    let get = util.promisify(bucket.get);
-    return get(id);
+    return new Promise((resolve, reject) => {
+        bucket.get(id, (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+    });
+};
 
 exports.getUsers = () => {
     let queryString = 'SELECT * FROM' +  DB_NAME + 'WHERE type=$1';
-    let query = util.promisify(bucket.query);
-    return query(couchbase.N1qlQuery.fromString(queryString), ['user']);
+    return new Promise((resolve, reject) => {
+        bucket.query(couchbase.N1qlQuery.fromString(queryString), ['user'], (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+    });
 };
 
 exports.createUser = (user) => {
-    let insert = util.promisify(bucket.insert);
-    return insert(user.key, user);
+    return new Promise((resolve, reject) => {
+        bucket.insert(user.key, user, (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+    });
 };
 
 exports.updateUser = (user) => {
-    let upsert = util.promisify(bucket.upsert);
-    return upsert(user.key, user);
+    return new Promise((resolve, reject) => {
+        bucket.upsert(user.key, user, (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+    });
 };
