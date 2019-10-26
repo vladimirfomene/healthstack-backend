c6db8da89e08a2522d9bb2bfdec05c4b00b24b47
vladimirfomene <vladimirfomene@gmail.com>
2019-10-26 15:08:40 +0100

    move from promise based code to callbacks

--- config/database_creation.js
+++ config/database_creation.js
@@ -1,35 +1,44 @@
-const util = require('util');
 const { DB_HOST, DB_NAME } = require('../config/database_setup');
 const couchbase = require('couchbase');
-const bucket = (new couchbase.Cluster(DB_HOST)).openBucket(DB_NAME);
+const cluster = new couchbase.Cluster(DB_HOST);
+cluster.authenticate('Administrator', 'fomeneodiwuor');
+const bucket = cluster.openBucket(DB_NAME);
 
 let bktmanager = bucket.manager();
-let createPrimaryIndex = util.promisify(bktmanager.createPrimaryIndex);
-let createIndex = util.promisify(bktmanager.createIndex);
 
 //Primary INDEX
-createPrimaryIndex({ignoreExists: true})
-.then(result => {
-    console.log(result);
-})
-.catch(err => {
-    console.error(err);
-});
+/*bktmanager.createPrimaryIndex({ignoreExists: true}, (err, result) => {
+	if(result){
+    	console.log(result);
+	}else{
+    	console.error(err);
+	}
+});*/
+
 
 //Email INDEX
-createIndex('email_index', ['email'])
-.then(result => {
-    console.log(result)
-})
-.catch(err => {
-    console.error(err);
-});
+/*bktmanager.createIndex('email_index', ['email'], (err, result) => {
+	if(result){
+		console.log(result)
+	}else{
+		console.error(err);
+	}
+});*/
 
 //Name INDEX
-createIndex('name_index', ['name'])
-.then(result => {
-    console.log(result)
-})
-.catch(err => {
-    console.error(err);
+/*bktmanager.createIndex('name_index', ['name'], (err, result) => {
+	if(result){
+		console.log(result)
+	}else{
+		console.error(err);
+	}
+});*/
+
+//tel INDEX
+bktmanager.createIndex('tel_index', ['telephone'], (err, result) => {
+	if(result){
+		console.log(result)
+	}else{
+		console.error(err);
+	}
 });
