5faf0a15aae01a5ed22a637e1b35bd0345bd1b41
vladimirfomene <vladimirfomene@gmail.com>
2019-10-29 14:43:32 +0100

    correct query for get user by email and add password hashing in createUser with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 const { DB_HOST, DB_NAME } = require('../config/database_setup');
 const couchbase = require('couchbase');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const cluster = new couchbase.Cluster(DB_HOST);
 cluster.authenticate('Administrator', 'fomeneodiwuor');
 const bucket = cluster.openBucket(DB_NAME);
 
 exports.getUserByEmail = (email) => {
-    console.log(email)
-    let queryString = 'SELECT * FROM `' +  DB_NAME + '` WHERE type=$1 AND LOWER(email) LIKE \'%' + email.toLowerCase() + '%\'';
+    let queryString = 'SELECT * FROM `' +  DB_NAME + '` WHERE type=$1 AND email=$2';
     return new Promise((resolve, reject) => {
-        bucket.query(couchbase.N1qlQuery.fromString(queryString), ['user'], (err, result) => {
+        bucket.query(couchbase.N1qlQuery.fromString(queryString), ['user', email], (err, result) => {
             if(err) reject(err);
             resolve(result);
         });
@@ -35,6 +36,7 @@ exports.getUsers = () => {
 };
 
 exports.createUser = (user) => {
+    user.password = bcrypt.hashSync(user.password, saltRounds);
     return new Promise((resolve, reject) => {
         bucket.insert(user.key, user, (err, result) => {
             if(err) reject(err);
